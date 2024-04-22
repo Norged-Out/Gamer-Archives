07c4e376b0a9951a6592fc55246e527c0250d473
Priyansh Nayak <priyansh.nayak.28@gmail.com>
2024-04-22 16:30:12 -0700

    Updating index builder

--- src/main/java/edu/arizona/cs/WatsonIndex.java
+++ src/main/java/edu/arizona/cs/WatsonIndex.java
@@ -1,4 +1,6 @@
+
 package edu.arizona.cs;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -22,25 +24,39 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The WatsonIndex class represents an index builder for Watson documents.
+ * It uses Apache Lucene library for indexing and searching documents.
+ * The class provides methods to build a new index from a given input file,
+ * parse text files, and add documents to the index.
+ */
 public class WatsonIndex {
     boolean indexExists = false;
     String indexFilePath = "";
-    StandardAnalyzer analyzer = null;
+    StandardAnalyzer oldAnalyzer = null; // Not using it anymore
+    EnglishAnalyzer analyzer = null;  // This is better for parsing English text
     Directory index = null;
     IndexWriterConfig config = null;
     IndexWriter writer = null;
 
+    /**
+     * Constructs a WatsonIndex object with the specified input file.
+     * @param inputFile the path of the input file
+     */
     public WatsonIndex(String inputFile) {
         indexFilePath = inputFile;
         buildNewIndex();
     }
 
+    /**
+     * Builds a new index using the specified input file.
+     */
     private void buildNewIndex() {
         //Get file from resources folder
-        analyzer = new StandardAnalyzer();
+        analyzer = new EnglishAnalyzer();
         //index = new ByteBuffersDirectory();
         config = new IndexWriterConfig(analyzer);
-        
+
         try {
             index = FSDirectory.open(Paths.get(indexFilePath));
         } catch (IOException e) {
@@ -62,7 +78,82 @@ public class WatsonIndex {
         indexExists = true;
     }
 
-    private void parseTextFile(IndexWriter writer, String filePath) {
+    /**
+     * Parses a text file and returns its content as a string.
+     * @param filePath the path of the text file
+     * @return the content of the text file
+     */
+    private String parseTextFile(String filePath){
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            // Open the file
+            File file = new File(filePath);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            // Read the file line by line and append to the content builder
+            String line;
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+
+            // Close the reader
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
+    }
+
+    /**
+     * Adds all text files in the resources directory to the index.
+     * @param writer the IndexWriter object
+     */
+    private void allFilesToProcess(IndexWriter writer) {
+        String resourcesPath = "src/main/resources";
+        // Get the resources directory
+        File resourcesDirectory = new File(resourcesPath);
+        String directoryName = null; 
+        // Check if it exists and is a directory
+        if (resourcesDirectory.exists() && resourcesDirectory.isDirectory()) {
+            // Get all subdirectories in the resources directory
+            File[] subDirectories = resourcesDirectory.listFiles(File::isDirectory);
+            // Iterate over each subdirectory
+            for (File subDirectory : subDirectories) {
+                directoryName = subDirectory.getName();
+                System.out.println("Directory: " + directoryName);
+                // Get all text files in the subdirectory
+                File[] textFiles = subDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
+                // Iterate over each text file
+                List<String> fileContents = new ArrayList<>();
+                for (File textFile : textFiles) {
+                    System.out.println("  Text File: " + textFile.getName());
+                    String fileContent = parseTextFile(textFile.getPath());
+                    fileContents.add(fileContent);
+                }
+                String accumulatedContent = String.join("", fileContents);
+                // Add the accumulated content to the index
+                try {
+                    Document newDoc = new Document();
+                    newDoc.add(new StringField("docName", directoryName, Field.Store.YES));
+                    newDoc.add(new TextField("docData", accumulatedContent, Field.Store.YES));
+                    writer.addDocument(newDoc);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("Resources directory does not exist or is not a directory.");
+        }
+    }
+
+    /*
+     * This function acted as an earlier version of the parser. It is not used anymore.
+     * It was used to parse a single text file and extract documents from it.
+     * It obeyed the format provided in the Watson specification.
+     * @param writer the IndexWriter object
+     * @param filePath the path of the text file
+     */
+    private void oldParser(IndexWriter writer, String filePath) {
         StringBuilder contentBuilder = new StringBuilder();
         String docName = null;
         
@@ -103,41 +194,17 @@ public class WatsonIndex {
                 newDoc.add(new TextField("docData", contentBuilder.toString(), Field.Store.YES));
                 writer.addDocument(newDoc);
             }
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         
     }
 
-    private void allFilesToProcess(IndexWriter writer) {
-        String resourcesPath = "src/main/resources";
-        // Get the resources directory
-        File resourcesDirectory = new File(resourcesPath);
-        String directoryName = null;
-        String fileName = null;       
-        // Check if it exists and is a directory
-        if (resourcesDirectory.exists() && resourcesDirectory.isDirectory()) {
-            // Get all subdirectories in the resources directory
-            File[] subDirectories = resourcesDirectory.listFiles(File::isDirectory);
-            // Iterate over each subdirectory
-            for (File subDirectory : subDirectories) {
-                // Print the name of the subdirectory
-                directoryName = subDirectory.getName();
-                System.out.println("Directory: " + directoryName);
-                // Get all text files in the subdirectory
-                File[] textFiles = subDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
-                // Iterate over each text file and print its name
-                for (File textFile : textFiles) {
-                    fileName = textFile.getName();
-                    System.out.println("  Text File: " + fileName);
-                    parseTextFile(writer, textFile.getPath());
-                }
-            }
-        } else {
-            System.out.println("Resources directory does not exist or is not a directory.");
-        }
-    }
-
+    /**
+     * The main method of the WatsonIndex program.
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         WatsonIndex engine = new WatsonIndex("testindex");
@@ -153,7 +220,6 @@ public class WatsonIndex {
             }
             reader.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
